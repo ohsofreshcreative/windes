7ec77f3a4cb5cde90b97cc7128fe5e1a23c76568
Arek <kontakt@ohsofresh.pl>
2025-09-26 10:58:25 +0200

    Stop tracking vendor directory

--- vendor/log1x/acf-composer/resources/views/alpine-support.blade.php
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript">
-  acf.addFilter('acf_blocks_parse_node_attr', (current, node) => (node.name.startsWith('x-') || node.name.startsWith('@')) ? node : current);
-</script>
