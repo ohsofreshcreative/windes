7bc5b7878f13aff6471512a8a30794b86ed2ef76
Arek <kontakt@ohsofresh.pl>
2026-07-28 05:52:49 +0200

    Automatyczny deploy

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -5,7 +5,7 @@ use App\Walkers\MobileDropdownWalker;
 
 <header x-data="{ mobileOpen: false }" class="relative top-0 z-50 bg-dark masthead fixed-top">
 	<div class="__topbar w-full py-2 px-12 mx-auto flex items-center justify-between">
-		<div class="__logo flex gap-4">
+		<div class="__logo  gap-4 hidden md:flex">
 			<p class="text-white">Jesteśmy oficjalnym dystrybutorem firmy </p>
 			<img src="/wp-content/uploads/2026/03/lomax.svg" />
 		</div>
